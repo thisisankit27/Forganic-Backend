1188b2d3e601e2450c99460ec50fec12c1a8a895
Ankit Srivastava <69184999+thisisankit27@users.noreply.github.com>
2023-12-21 08:17:18 +0530

    Mishandled Cors Headers Removed (#4)
    Signed-off-by: Ankit Srivastava <thisisankitsrivastava@gmail.com>

--- courierAPI/courierAPI/settings.py
+++ courierAPI/courierAPI/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'PriceCalculator',
     'PlaceOrder',
@@ -51,11 +52,14 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'courierAPI.urls'
 
